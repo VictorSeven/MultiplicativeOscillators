245aa2645ac09d38da52ec49b37b4a6fa1682a25
Victor <vbuendiar@gmail.com>
2023-06-05 20:45:06 +0200

    More accurate tests to the theory

--- src/julia/graphs/figure2.jl
+++ src/julia/graphs/figure2.jl
@@ -12,6 +12,32 @@ function angle_diff(angle1, angle2)
     return @. min(diff, 2π - diff) 
 end
 
+function diff_random_angles(n_angles, its, nharms)
+    k=collect(1:nharms)
+    av_diff = zeros(nharms)
+    for i=1:its 
+        phis = 2π*rand(n_angles)
+        zk = [mean(exp.(im*phis*k)) for k=1:nharms]
+        psi = angle.(zk)
+        av_diff += angle_diff.(psi, k*psi[1])
+    end
+    av_diff /= its
+    return mod.(av_diff, 2π)
+end
+
+function diff_gaussian_angles(n_angles, its, nharms)
+    k=collect(1:nharms)
+    av_diff = zeros(nharms)
+    for i=1:its 
+        phis = 2*π*rand() .+ 0.5 * randn(n_angles)
+        zk = [mean(exp.(im*phis*k)) for k=1:nharms]
+        psi = angle.(zk)
+        av_diff += angle_diff.(psi, k*psi[1])
+    end
+    av_diff /= its
+    return mod.(av_diff, 2π)
+end
+
 function analyse_angle_recurrence(data_path, ax, label)
 
     xy = readdlm(data_path)
@@ -32,7 +58,7 @@ function analyse_angle_recurrence(data_path, ax, label)
     k = collect(1:nharms) 
     scatter!(ax, k, psidiff, markersize=5, label=label)
 
-    ylims!(ax, 0.0, 2π)
+    ylims!(ax, 0.0, π)
 end
 
 fig = Figure(resolution=one_col_size(2), fontsize=9, figure_padding=5)
@@ -42,14 +68,24 @@ xgridvisible=false, ygridvisible=false)
 hidespines!(ax, :t, :r)
 
 #data_path = "../../../data/timeseries/series_micro/series_100/series_0.13_1" 
-data_path = "../../../data/timeseries/time_micro_asyn_100_4"
+#data_path = "../../../data/timeseries/time_micro_asyn_100_4"
+data_path = "../../../data/series_micro/size_100/series_long_0.07"
 analyse_angle_recurrence(data_path, ax, "Kuramoto")
 
-data_path = "../../../data/timeseries/time_micro_asyn_EXC_100_1"
-analyse_angle_recurrence(data_path, ax, "Excitable Osc.")
+#data_path = "../../../data/timeseries/time_micro_asyn_EXC_100_1"
+#analyse_angle_recurrence(data_path, ax, "Excitable Osc.")
+
+#scatter!(ax, k, diff_random_angles(100, 10000, 10), markersize=5, label="Random")
+#scatter!(ax, k, diff_gaussian_angles(10000, 10000, 10), markersize=5, label="Gaussian")
+
+data_path = "../../../data/series_micro/size_100/series_long_0.2"
+analyse_angle_recurrence(data_path, ax, "Synchro")
+
+data_path = "../../../data/series_micro/size_10000/series_long_0.2"
+analyse_angle_recurrence(data_path, ax, "Synchro")
 
 hlines!(ax, π/2, color=:gray, linestyle=:dash, label=L"\pi/2")
 
-create_legend(ax, (0.005, 0.9))
+create_legend(ax, (0.005, 1.2))
 
 save("figure2.pdf", fig, pt_per_unit=1)
--- src/julia/graphs/theory_formulas.jl
+++ src/julia/graphs/theory_formulas.jl
@@ -1,3 +1,5 @@
+using Statistics
+
 function r_6th(q, s2)
     ratio = @. s2 * sqrt(2) / q
     root = @. sqrt(complex(51*q*q - 132 * q * s2 + 306*s2*s2)) 
@@ -20,4 +22,55 @@ function finite_size_r(q, s2, n)
     aux = @. q * n - s2 * (n - 1)  
     aux = @. aux + sqrt(4*n*q*s2 + aux^2)
     return @. sqrt(aux / (2*q*n))
+end
+
+function tyulkina(w, q, s2, oldz, oldx, dt)
+    z = oldz + dt * ( 0.5*(q-s2+2*im*w)*oldz - 0.5*q*(oldz * abs2(oldz) + conj(oldz)*oldx)) 
+    x = oldx + dt * ( 2*im*w*oldx - s2*(2*oldx + oldz^2) - 2*q*oldx*abs2(oldz) )
+    return z,x
+end
+
+function full_system(nharm, w, q, s2, oldz, z, dt)
+    k = 1 #z[0]=1
+    z[k] = oldz[k] + dt *( oldz[k] * (im*w*k - 0.5*k*k*s2) + 0.5*q*k*(oldz[1] - conj(oldz[1])*oldz[k+1]))
+    @simd for k=2:nharm-1
+        z[k] = oldz[k] + dt *( oldz[k] * (im*w*k - 0.5*k*k*s2) + 0.5*q*k*(oldz[1]*oldz[k-1] - conj(oldz[1])*oldz[k+1]))
+    end
+    k = nharm #z[nharm+1]=0
+    z[k] = oldz[k ] + dt * (oldz[k] * (im*w*k - 0.5*k*k*s2) + 0.5*q*k*oldz[1]*oldz[k-1] )
+end
+
+function integrate_tyulkina(w, q, s2; dt=0.01, tf=1000.0, ntrials=10)
+    avr = 0.0 
+    @fastmath @inbounds for i=1:ntrials
+        angles = 2π*rand(100000)
+        oldz = mean(exp.(im*angles))
+        z2 = mean(exp.(2*im*angles))
+        oldx = z2 - oldz.^2  
+
+        z = 0.0
+        for t=0.0:dt:tf 
+            z, x = tyulkina(w, q, s2, oldz, oldx, dt)
+            oldz, oldx = z, x
+        end
+        avr += abs(z)
+    end
+
+    return avr/ntrials 
+end
+
+function integrate_full(w, q, s2; dt=0.01, tf=1000.0, ntrials=1, nharm=20)
+    avr = 0.0 
+    @fastmath @inbounds for i=1:ntrials
+        angles = 2π*rand(100000)
+        oldz = [mean(exp.(im*angles*k)) for k=1:nharm]
+        z = Vector{ComplexF64}(undef, nharm)
+        for t=0.0:dt:tf 
+            full_system(nharm, w, q, s2, oldz, z, dt)
+            oldz, z = z, oldz 
+        end
+        avr += abs(oldz[1])
+    end
+
+    return avr/ntrials 
 end
